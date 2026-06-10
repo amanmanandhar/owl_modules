9f078e20591d5013692b0b78be94ae6e708421fa
Aman <amansenpai01@gmail.com>
2026-06-10 15:41:31 +0545

    delete function orm added with notification

--- user_list_orm/static/src/js/user.js
+++ user_list_orm/static/src/js/user.js
@@ -8,6 +8,7 @@ export class UserList extends Component {
     static template = "user_list_orm.Template";
     setup(){
         this.orm = useService("orm");
+        this.notification = useService("notification");
         this.state = useState({
             users: [],
         });
@@ -16,7 +17,20 @@ export class UserList extends Component {
         });
     }
     async loadEmployees(){
-        this.state.users = await this.orm.searchRead("res.users",[],["name","email"]);
+        this.state.users = await this.orm.searchRead("res.users",[],["name","email","share"]);
+    }
+    async deleteUser(user){
+        if(!user.share){
+            this.notification.add("Cannot delete the internal user",{
+                type: "danger",
+            });
+            return;
+        }
+        await this.orm.unlink('res.users',[user.id]);
+        this.notification.add("User deleted successfully",{
+            type: "success",
+        });
+        await this.loadEmployees()
     }
 }
 registry.category('public_components').add('user_list_view', UserList)
