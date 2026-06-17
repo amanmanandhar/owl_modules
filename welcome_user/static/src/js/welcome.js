8fc5587d652eafbf3a67b8e2fcbf367c8eb28cbc
Aman <amansenpai01@gmail.com>
2026-06-17 18:09:21 +0545

    corrected js

--- welcome_user/static/src/js/welcome.js
+++ welcome_user/static/src/js/welcome.js
@@ -1,14 +1,18 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { rpc } from "@web/core/network/rpc";
 
 const welcomeService = {
     dependencies: ["action"],
 
     async start(env, { action }) {
-        setTimeout(() => {
-            action.doAction("welcome_user.action_welcome");
-        }, 2000);
+        const showWelcome = await rpc('/welcome_user/check', {});
+        if (showWelcome) {
+            setTimeout(() => {
+                action.doAction("welcome_user.action_welcome");
+            }, 2000);
+        }
     },
 };
 
