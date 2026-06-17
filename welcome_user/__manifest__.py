fd8588801ef83bd77160442a90ba5c6e1dcef9b3
Aman <amansenpai01@gmail.com>
2026-06-17 18:13:35 +0545

    welcome module finished

--- welcome_user/__manifest__.py
+++ welcome_user/__manifest__.py
@@ -1,35 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "welcome_user",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'name': 'Welcome After Login',
+    'summary': 'Dialog learning, small module',
     'description': """
-Long description of module's purpose
+        A prototype to show the things or model dialog after login can make a different age verification or different dialog with same type of logic
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'author': 'Aman Manandhar',
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'version': '18.0.1.0.0',
     'depends': ['base'],
     'assets': {
         'web.assets_backend': [
             'welcome_user/static/src/js/welcome.js',
         ],
     },
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
         'views/welcome_wizard.xml',
     ],
     'installable': True,
