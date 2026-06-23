8ab8e49fe8188edf3f5d279d42d68736a74228a0
Aman <amansenpai01@gmail.com>
2026-06-23 17:44:41 +0545

    finished and final

--- throttle_debounce/__manifest__.py
+++ throttle_debounce/__manifest__.py
@@ -1,23 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': "throttle_debounce",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'summary': "Module to see difference between throttle and debounce",
     'description': """
-Long description of module's purpose
+        Module to see difference between throttle and debounce using the fakeAPI call
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'author': "Aman Manandhar",
+    'category': 'Website',
+    'version': '18.0.1.0.0',
     'depends': ['website'],
     'assets': {
         'web.assets_frontend': [
@@ -26,11 +16,7 @@ Long description of module's purpose
             'throttle_debounce/static/src/scss/throttle_debounce.scss',
         ],
     },
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
         'views/throttle_debounce_views.xml',
     ],
     'installable': True,
